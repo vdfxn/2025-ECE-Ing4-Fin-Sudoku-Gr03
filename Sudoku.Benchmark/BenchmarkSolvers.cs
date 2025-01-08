a2999ce075e507c790e9bcd051ce7b953e18f032
jsboigeECE <97412849+jsboigeECE@users.noreply.github.com>
2025-01-08 03:12:15 +0100

    Initial commit

---
+++ Sudoku.Benchmark/BenchmarkSolvers.cs
@@ -0,0 +1,260 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Exporters.Csv;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Mathematics;
+using BenchmarkDotNet.Order;
+using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Toolchains.CoreRun;
+using BenchmarkDotNet.Toolchains.InProcess.Emit;
+using BenchmarkDotNet.Toolchains.InProcess.NoEmit;
+using BenchmarkDotNet.Toolchains.Roslyn;
+using Sudoku.Shared;
+
+
+
+namespace Sudoku.Benchmark
+{
+
+    public class QuickBenchmarkSolversHard : QuickBenchmarkSolversEasy
+    {
+        public QuickBenchmarkSolversHard() : base()
+		{
+            NbPuzzles = 10;
+            MaxSolverDuration = TimeSpan.FromSeconds(30);
+			Difficulty = SudokuDifficulty.Hard;
+        }
+    }
+
+
+    public class QuickBenchmarkSolversMedium : QuickBenchmarkSolversEasy
+    {
+        public QuickBenchmarkSolversMedium() : base()
+		{
+            NbPuzzles = 10;
+            MaxSolverDuration = TimeSpan.FromSeconds(20);
+			Difficulty = SudokuDifficulty.Medium;
+        }
+    }
+
+
+    [Config(typeof(Config))]
+    public class QuickBenchmarkSolversEasy : BenchmarkSolversBase
+    {
+        public QuickBenchmarkSolversEasy() : base()
+		{
+            MaxSolverDuration = TimeSpan.FromSeconds(10);
+            NbPuzzles = 2;
+        }
+		private class Config : SudokuBenchmarkConfigBase
+        {
+			public Config(): base()
+            {
+				var baseJob = GetBaseJob()
+                    .WithIterationCount(1)
+					.WithInvocationCount(1);
+				this.AddJob(baseJob);
+            }
+        }
+
+        public override SudokuDifficulty Difficulty { get; set; } = SudokuDifficulty.Easy;
+
+    }
+
+
+    [Config(typeof(Config))]
+    public class CompleteBenchmarkSolvers : BenchmarkSolversBase
+    {
+
+        public CompleteBenchmarkSolvers(): base()
+        {
+            MaxSolverDuration = TimeSpan.FromMinutes(1);
+        }
+
+		private class Config : SudokuBenchmarkConfigBase
+		{
+			public Config() : base()
+			{
+				var baseJob = GetBaseJob();
+				this.AddJob(baseJob);
+			}
+		}
+
+		[ParamsAllValues]
+		public override SudokuDifficulty Difficulty { get; set; }
+
+
+	}
+
+
+	public abstract class SudokuBenchmarkConfigBase : ManualConfig
+	{
+
+		public SudokuBenchmarkConfigBase()
+        {
+			if (Program.IsDebug)
+            {
+                Options |= ConfigOptions.DisableOptimizationsValidator;
+			}
+				this.AddColumnProvider(DefaultColumnProviders.Instance);
+				this.AddColumn(new RankColumn(NumeralSystem.Arabic));
+			
+			this.AddLogger(ConsoleLogger.Default);
+			//this.AddExporter(new CsvExporter(CsvSeparator.Comma, SummaryStyle.Default));
+			this.UnionRule = ConfigUnionRule.AlwaysUseLocal;
+
+		}
+
+		public static Job GetBaseJob()
+		{
+			var baseJob = Job.Dry
+                    .WithId("Solving Sudokus")
+                    .WithPlatform(Platform.X64)
+					//.WithJit(Jit.Default)
+					.WithJit(Jit.RyuJit)
+					.WithRuntime(CoreRuntime.Core80)
+                    .WithLaunchCount(1)
+                    .WithWarmupCount(1)
+                    .WithIterationCount(3)
+                    .WithInvocationCount(3)
+                    
+				.WithUnrollFactor(1);
+			//         if (Program.IsDebug)
+			//         {
+			//             baseJob = baseJob.WithCustomBuildConfiguration("Debug");
+			//             baseJob = baseJob.WithToolchain(InProcessEmitToolchain.Instance);
+			//}
+
+			//baseJob = baseJob.WithCustomBuildConfiguration("Debug");
+			//baseJob = baseJob.WithToolchain(InProcessEmitToolchain.Instance);
+			baseJob = baseJob.WithToolchain(InProcessNoEmitToolchain.Instance);
+			return baseJob;
+		}
+    }
+
+
+
+
+
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [MinColumn, MaxColumn, MeanColumn, MedianColumn]
+    public abstract class BenchmarkSolversBase
+    {
+
+        static BenchmarkSolversBase()
+        {
+			
+            _Solvers = new[] { new EmptySolver() }.Concat(Shared.SudokuGrid.GetSolvers().Select(s =>
+            {
+	            try
+	            {
+		            return s.Value.Value;
+				}
+	            catch (Exception e)
+	            {
+		            Console.WriteLine(e);
+		            return new EmptySolver();
+	            }
+	            
+            }).Where(s => s.GetType() != typeof(EmptySolver))).Select(s => new SolverPresenter() { Solver = s }).ToList();
+			//_Solvers = SudokuGrid.GetSolvers().Where(s => ! s.Value.Value.GetType().Name.ToLowerInvariant().StartsWith("pso")).Select(s => new SolverPresenter() { Solver = s.Value.Value }).ToList();
+			
+        }
+
+
+        [GlobalSetup]
+        public virtual void GlobalSetup()
+        {
+            AllPuzzles = new Dictionary<SudokuDifficulty, IList<Shared.SudokuGrid>>();
+            foreach (var difficulty in Enum.GetValues(typeof(SudokuDifficulty)).Cast<SudokuDifficulty>())
+            {
+                AllPuzzles[difficulty] = SudokuHelper.GetSudokus(Difficulty);
+            }
+
+        }
+
+		private static SudokuGrid _WarmupSudoku = SudokuGrid.ReadSudoku("483921657967345001001806400008102900700000008006708200002609500800203009005010300");
+
+        [IterationSetup]
+        public void IterationSetup()
+        {
+            IterationPuzzles = new List<Shared.SudokuGrid>(NbPuzzles);
+            for (int i = 0; i < NbPuzzles; i++)
+            {
+                IterationPuzzles.Add(AllPuzzles[Difficulty][i].CloneSudoku());
+            }
+
+			try
+			{
+				SolverPresenter.SolveWithTimeLimit(_WarmupSudoku, TimeSpan.FromSeconds(10));
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e);
+			}
+
+        }
+
+        private static readonly Stopwatch Clock = Stopwatch.StartNew();
+
+        public TimeSpan MaxSolverDuration;
+
+        public int NbPuzzles { get; set; } = 10;
+
+        public virtual SudokuDifficulty Difficulty { get; set; }
+
+        public IDictionary<SudokuDifficulty, IList<Shared.SudokuGrid>> AllPuzzles { get; set; }
+        public IList<Shared.SudokuGrid> IterationPuzzles { get; set; }
+
+        [ParamsSource(nameof(GetSolvers))]
+        public SolverPresenter SolverPresenter { get; set; }
+
+
+		private static IList<SolverPresenter> _Solvers;
+
+
+
+        public IEnumerable<SolverPresenter> GetSolvers()
+        {
+            return _Solvers;
+
+        }
+
+
+        [Benchmark(Description = "Benchmarking GrilleSudoku Solvers")]
+        public void Benchmark()
+        {
+            foreach (var puzzle in IterationPuzzles)
+            {
+                try
+                {
+                    Console.WriteLine($"//Solver {SolverPresenter} solving sudoku: \n {puzzle}");
+                    var startTime = Clock.Elapsed;
+                    var solution = SolverPresenter.SolveWithTimeLimit(puzzle, MaxSolverDuration);
+                    if (!solution.IsValid(puzzle))
+                    {
+                        throw new ApplicationException($"sudoku has {solution.NbErrors(puzzle)} errors");
+                    }
+
+                    var duration = Clock.Elapsed - startTime;
+                    var durationSeconds = (int)duration.TotalSeconds;
+                    var durationMilliSeconds = duration.TotalMilliseconds - (1000 * durationSeconds);
+                    Console.WriteLine($"//Valid Solution found: \n {solution} \n Solver {SolverPresenter} found the solution  in {durationSeconds} s {durationMilliSeconds} ms");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
+        }
+
+    }
+}
---
+++ Sudoku.Shared/PythonSolverBase.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Python.Deployment;
+using Python.Runtime;
+
+namespace Sudoku.Shared
+{
+    public abstract class PythonSolverBase : ISudokuSolver
+    {
+
+        static PythonSolverBase()
+        {
+
+
+        }
+
+        public PythonSolverBase()
+        {
+            if (!pythonInstalled)
+            {
+                InstallPythonComponents();
+                pythonInstalled = true;
+            }
+            InitializePythonComponents();
+        }
+
+        private static bool pythonInstalled = false;
+
+        public static void InstallPythonComponents()
+        {
+			Task task = Task.Run(() => InstallPythonComponentsAsync());
+			task.Wait();
+
+		}
+
+		protected static async Task InstallPythonComponentsAsync()
+		{
+
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+
+                await InstallMac();
+            }
+            else
+            {
+                await InstallEmbedded();
+            }
+		}
+
+		public static void InstallPipModule(string moduleName, string version = "", bool force = false)
+		{
+			Task task = Task.Run( () => InstallPipModuleAsync(moduleName, version, force));
+			task.Wait();
+
+		}
+
+
+		private static async Task InstallPipModuleAsync(string moduleName, string version = "", bool force = false)
+        {
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                await MacInstaller.PipInstallModule(moduleName, version, force);
+            }
+            else
+            {
+                await Installer.PipInstallModule(moduleName, version, force);
+            }
+        }
+
+        private static async Task InstallMac()
+        {
+
+            Console.WriteLine($"PythonDll={MacInstaller.LibFileName}");
+            Runtime.PythonDLL = MacInstaller.LibFileName;
+
+            MacInstaller.LogMessage += Console.WriteLine;
+            // Installer.SetupPython().Wait();
+
+            //MacInstaller.InstallPath = "/Library/Frameworks/Python.framework/Versions";
+            //MacInstaller.PythonDirectoryName = "3.7/";
+
+            var localInstallPath = MacInstaller.InstallPythonHome;
+            var existingPathEnvVar = Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.Machine);
+
+            var path = $"{localInstallPath}/lib;{localInstallPath};{existingPathEnvVar}";
+            var pythonPath = $"{localInstallPath}/lib/site-packages;{localInstallPath}/lib";
+
+            Environment.SetEnvironmentVariable("Path", path, EnvironmentVariableTarget.Process);
+            Console.WriteLine($"Path={Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.Process)}");
+
+            // Environment.SetEnvironmentVariable("PYTHONHOME", localInstallPath, EnvironmentVariableTarget.Process);
+            // Console.WriteLine($"PYTHONHOME={Environment.GetEnvironmentVariable("PYTHONHOME", EnvironmentVariableTarget.Process)}");
+            // Environment.SetEnvironmentVariable("PythonPath", pythonPath, EnvironmentVariableTarget.Process);
+            // Console.WriteLine($"PythonPath={Environment.GetEnvironmentVariable("PythonPath", EnvironmentVariableTarget.Process)}");
+
+
+            var aliasPath = $"{MacInstaller.LibFileName}";
+            if (!File.Exists(aliasPath))
+            {
+                var libPath = $"{localInstallPath}/lib/{MacInstaller.LibFileName}";
+                var aliasCommand =
+                    $"sudo ln -s {libPath} {aliasPath}";
+                Console.WriteLine($"run command={aliasCommand}");
+                MacInstaller.RunCommand(aliasCommand);
+            }
+
+
+
+            var dynamicLinkingCommnad = $@"export DYLD_LIBRARY_PATH={localInstallPath}/lib";
+            Console.WriteLine($"run command={dynamicLinkingCommnad}");
+            MacInstaller.RunCommand(dynamicLinkingCommnad);
+
+            // PythonEngine.PythonHome = localInstallPath;
+            // PythonEngine.PythonPath = pythonPath;
+
+            await MacInstaller.TryInstallPip();
+        }
+
+
+
+
+        private static async Task InstallEmbedded()
+        {
+
+            // // install in local directory. if you don't set it will install in local app data of your user account
+            //Python.Deployment.Installer.InstallPath = Path.GetFullPath(".");
+            //
+
+
+            //Runtime.PythonDLL = "python37.dll";
+            //Python.Deployment.Installer.Source = new Installer.DownloadInstallationSource()
+            //{
+            //    DownloadUrl = @"https://www.python.org/ftp/python/3.7.3/python-3.7.3-embed-amd64.zip",
+            //};
+
+            Runtime.PythonDLL = "python38.dll";
+            Python.Deployment.Installer.Source = new Installer.DownloadInstallationSource()
+            {
+                DownloadUrl = @"https://www.python.org/ftp/python/3.8.9/python-3.8.9-embed-amd64.zip",
+            };
+
+
+            //Runtime.PythonDLL = "python39.dll";
+            //Python.Deployment.Installer.Source = new Installer.DownloadInstallationSource()
+            //{
+            //    DownloadUrl = @"https://www.python.org/ftp/python/3.9.9/python-3.9.9-embed-amd64.zip",
+            //};
+
+            //Runtime.PythonDLL = "python37.dll";
+            // set the download source
+            //Python.Deployment.Installer.Source = new Installer.DownloadInstallationSource()
+            //{
+            //    DownloadUrl = @"https://www.python.org/ftp/python/3.7.3/python-3.7.3-embed-amd64.zip",
+            //};
+
+            //Runtime.PythonDLL = "python37.dll";
+            // set the download source
+            //Python.Deployment.Installer.Source = new Installer.DownloadInstallationSource()
+            //{
+            //    DownloadUrl = @"https://www.python.org/ftp/python/3.7.3/python-3.7.3-embed-amd64.zip",
+            //};
+
+
+            // // install in local directory. if you don't set it will install in local app data of your user account
+            //Python.Deployment.Installer.InstallPath = Path.GetFullPath(".");
+            //
+            // see what the installer is doing
+
+            Runtime.PythonDLL = "python310.dll";
+            Python.Deployment.Installer.Source = new Installer.DownloadInstallationSource()
+            {
+            	DownloadUrl = @"https://www.python.org/ftp/python/3.10.8/python-3.10.8-embed-amd64.zip",
+            };
+
+
+            //Runtime.PythonDLL = "python311.dll";
+            //Python.Deployment.Installer.Source = new Installer.DownloadInstallationSource()
+            //{
+            //    DownloadUrl = @"https://www.python.org/ftp/python/3.11.2/python-3.11.2-embed-amd64.zip",
+            //};
+
+
+            // see what the installer is doing
+            Installer.LogMessage += Console.WriteLine;
+            //
+            // install from the given source
+			await Python.Deployment.Installer.SetupPython();
+
+			await Installer.TryInstallPip();
+
+            //await InstallNumpy();
+			//Python.Deployment.Installer.SetupPython().Wait();
+			//Installer.TryInstallPip();
+
+        }
+
+        // Méthode pour exécuter une commande Python
+        // private static void RunPythonCommand(string pythonExecutable, string arguments)
+        // {
+        //     var processStartInfo = new ProcessStartInfo
+        //     {
+        //         FileName = pythonExecutable,
+        //         Arguments = arguments,
+        //         RedirectStandardOutput = true,
+        //         RedirectStandardError = true,
+        //         UseShellExecute = false,
+        //         CreateNoWindow = true
+        //     };
+
+        //     using (var process = new Process { StartInfo = processStartInfo })
+        //     {
+        //         process.Start();
+
+        //         // Capture et affiche la sortie
+        //         string output = process.StandardOutput.ReadToEnd();
+        //         string error = process.StandardError.ReadToEnd();
+
+        //         process.WaitForExit();
+
+        //         if (!string.IsNullOrEmpty(output))
+        //         {
+        //             Console.WriteLine($"Output: {output}");
+        //         }
+
+        //         if (!string.IsNullOrEmpty(error))
+        //         {
+        //             Console.WriteLine($"Error: {error}");
+        //         }
+        //     }
+        // }
+
+        // public static async Task InstallNumpy()
+        // {
+        //     string pythonExecutable = Path.Combine(
+        //         Python.Deployment.Installer.InstallPath,
+        //         "python.exe"
+        //     );
+
+        //     // Assurez-vous que `pip` est installé
+        //     RunPythonCommand(pythonExecutable, "-m pip install numpy");
+
+        //     // Vérifiez si numpy est installé
+        //     RunPythonCommand(pythonExecutable, "-c \"import numpy; print(numpy.__version__)\"");
+        // }
+
+
+        protected virtual void InitializePythonComponents()
+        {
+
+
+            PythonEngine.Initialize();
+            //dynamic sys = PythonEngine.ImportModule("sys");
+            //Console.WriteLine("Python version: " + sys.version);
+        }
+
+
+        public abstract Shared.SudokuGrid Solve(Shared.SudokuGrid s);
+
+
+        /// <summary>
+        /// Injecte le script de conversion dans le scope Python
+        /// </summary>
+		protected void AddNumpyConverterScript(PyModule scope)
+        {
+			string numpyConverterCode = Resources.numpy_converter_py;
+			scope.Exec(numpyConverterCode);
+		}
+
+
+        /// <summary>
+        /// Convertit un tableau .NET en tableau NumPy
+        /// </summary>
+		public static PyObject AsNumpyArray(int[,] sCells, PyModule scope)
+		{
+			var pyObject = sCells.ToPython();
+			PyObject asNumpyArray = scope.Get("asNumpyArray");
+			PyObject pyCells = asNumpyArray.Invoke(pyObject);
+			return pyCells;
+		}
+
+        /// <summary>
+        /// Convertit un tableau NumPy en tableau .NET
+        /// </summary>
+		public static int[,] AsManagedArray(PyModule scope, PyObject pyCells)
+        {
+            PyObject asNetArray = scope.Get("asNetArray");
+            PyObject netResult = asNetArray.Invoke(pyCells);
+
+            int[,] intArray;
+
+			try
+            {
+				intArray = netResult.AsManagedObject(typeof(int[,])) as int[,];
+            }
+            catch
+            {
+				// Convertissez le PyObject résultant en tableau .NET
+				var longArray = netResult.AsManagedObject(typeof(long[,])) as long[,];
+				if (longArray == null)
+				{
+					throw new InvalidCastException("Cannot convert the result to long[,].");
+				}
+
+				int rows = longArray.GetLength(0);
+				int cols = longArray.GetLength(1);
+				intArray = new int[rows, cols];
+
+				for (int i = 0; i < rows; i++)
+				{
+					for (int j = 0; j < cols; j++)
+					{
+						intArray[i, j] = (int)longArray[i, j];
+					}
+				}
+
+				
+			}
+            return intArray;
+
+		}
+
+
+	}
+
+}
