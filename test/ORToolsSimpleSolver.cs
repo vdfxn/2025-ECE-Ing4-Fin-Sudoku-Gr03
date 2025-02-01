1abf13ab64f89af92bf15b3ddfc26cea633e884e
Chturm <julespinte@gmail.com>
2025-02-01 11:43:31 +0100

    Initial commit

---
+++ test/ORToolsSimpleSolver.cs
@@ -0,0 +1,12 @@
+﻿using Sudoku.Shared;
+
+namespace Sudoku.ORToolsSimpleSolver
+{
+    public class ORToolsSimpleSolver : ISudokuSolver
+    {
+        public SudokuGrid Solve(SudokuGrid s)
+        {
+            return s;
+        }
+    }
+}
