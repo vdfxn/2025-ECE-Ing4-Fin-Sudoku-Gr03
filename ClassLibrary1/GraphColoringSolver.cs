d09da6c4076b19d0a34dde6bcb37821b3e1d159d
Chturm <julespinte@gmail.com>
2025-02-01 23:41:57 +0100

    GraphColoringSolver: ColorGraphGreedy et ColorGraphDSatur

--- ClassLibrary1/GraphColoringSolver.cs
+++ ClassLibrary1/GraphColoringSolver.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
+
 namespace Sudoku.GraphColoringSolver
 {
     public class GraphColoringSolver : ISudokuSolver
@@ -13,7 +14,8 @@ namespace Sudoku.GraphColoringSolver
             var graph = BuildSudokuGraph(s);
 
             // 2. Coloration du graphe (gloutonne pour commencer)
-            var coloredGrid = ColorGraphGreedy(s, graph);
+            //var coloredGrid = ColorGraphGreedy(s, graph);
+            var coloredGrid = ColorGraphDSatur(s, graph);
           
              if (coloredGrid.IsValid(s))
              {
@@ -98,5 +100,97 @@ namespace Sudoku.GraphColoringSolver
             return sudokuGrid;
         }
 
+
+        private SudokuGrid ColorGraphDSatur(SudokuGrid s, Dictionary<(int, int), List<(int, int)>> graph)
+        {
+            var sudokuGrid = s.CloneSudoku();
+            var colors = new Dictionary<(int, int), int>();
+            var saturationDegrees = new Dictionary<(int, int), int>();
+
+
+            // Initialisation des dictionnaires
+            foreach (var vertex in graph.Keys)
+            {
+                colors[vertex] = 0; // 0 indique que le sommet n'est pas encore coloré
+                saturationDegrees[vertex] = 0; // Initialisation du degré de saturation à 0 pour tous les sommets
+
+                if (sudokuGrid.Cells[vertex.Item1, vertex.Item2] != 0)
+                {
+                    colors[vertex] = sudokuGrid.Cells[vertex.Item1, vertex.Item2];
+                }
+            }
+
+            // Boucle principale de l'algorithme DSatur
+            while (colors.Any(kvp => kvp.Value == 0))
+            {
+                // 1. Mise à jour des degrés de saturation
+                UpdateSaturationDegrees(graph, colors, saturationDegrees);
+
+
+                // 2. Selection du sommet non coloré avec le plus grand degré de saturation
+                var vertexToColor = saturationDegrees
+                    .Where(kvp => colors[kvp.Key] == 0)
+                    .OrderByDescending(kvp => kvp.Value)
+                    .ThenBy(kvp => graph[kvp.Key].Count) // En cas d'égalité on préfère le noeud de plus haut degré
+                    .FirstOrDefault().Key;
+
+
+                // 3. Coloration du sommet choisi avec la plus petite couleur possible
+                if (vertexToColor != (0,0))
+                {
+                    var usedColors = new HashSet<int>();
+                    foreach (var neighbor in graph[vertexToColor])
+                    {
+                        if (colors.ContainsKey(neighbor) && colors[neighbor] != 0)
+                        {
+                            usedColors.Add(colors[neighbor]);
+                        }
+                    }
+
+                    for (int color = 1; color <= 9; color++)
+                    {
+                        if (!usedColors.Contains(color))
+                        {
+                            colors[vertexToColor] = color;
+                            break;
+                        }
+                    }
+                } else
+                {
+                   break; //Si tous les sommets non colorés sont de degré 0, il n'y a pas de sommet a colorer
+                }
+
+
+            }
+
+            // Copie des couleurs dans la grille de Sudoku
+            foreach (var vertex in graph.Keys)
+            {
+                sudokuGrid.Cells[vertex.Item1, vertex.Item2] = colors[vertex];
+            }
+            return sudokuGrid;
+        }
+
+
+        private void UpdateSaturationDegrees(Dictionary<(int, int), List<(int, int)>> graph, Dictionary<(int, int), int> colors, Dictionary<(int, int), int> saturationDegrees)
+        {
+            foreach (var vertex in graph.Keys)
+            {
+                saturationDegrees[vertex] = 0;
+                if (colors[vertex] == 0) // On calcule le degré de saturation que pour les noeuds non colorés
+                {
+                    var usedColors = new HashSet<int>();
+                    foreach (var neighbor in graph[vertex])
+                    {
+                         if (colors.ContainsKey(neighbor) && colors[neighbor] != 0)
+                         {
+                            usedColors.Add(colors[neighbor]);
+                        }
+                    }
+                    saturationDegrees[vertex] = usedColors.Count;
+                }
+
+            }
+        }
     }
 }
