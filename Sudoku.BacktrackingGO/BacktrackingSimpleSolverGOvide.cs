5464e5312992dc4f34ac375f55042cfdae0cd072
gilou71 <gilles.ollivier24@gmail.com>
2025-02-02 20:08:44 +0100

    Ajout "résolution" vide + backtracking fonctionnel
    Ajout de la méthode qui copie la grille d'entrée et l'envoie telle qu'elle en sortie (fichier "BacktrackingSimpleSolverGOvide.cs") et ajout d'un Backtracking qui résoud les grilles "easy" en ~15ms, les "medium" en ~50-60 ms et les "hard" à ~130ms. Il s'agit des fichiers "BacktrackingSimpleSolver.py" et "BackTrackingSmplePythonSolverGO.cs"

---
+++ Sudoku.BacktrackingGO/BacktrackingSimpleSolverGOvide.cs
@@ -0,0 +1,11 @@
+﻿using Sudoku.Shared;
+
+namespace Sudoku.BacktrackingGO;
+
+public class BacktrackingSimpleSolverGOvide : ISudokuSolver
+{
+    public SudokuGrid Solve(SudokuGrid s)
+    {
+        return s;
+    }
+}
