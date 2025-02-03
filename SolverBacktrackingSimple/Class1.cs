b4ff4d22d3b8a748ce6efff655b007bfe4d562b7
Paul Prochet <134956499+Paul-prch@users.noreply.github.com>
2025-02-03 17:47:16 +0100

    optimisation du solver avec backtracking

--- SolverBacktrackingSimple/Class1.cs
+++ SolverBacktrackingSimple/Class1.cs
@@ -1,23 +1,20 @@
 ﻿using Sudoku.Shared;
+using System;
+using System.Collections.Generic;
 
-namespace SolverBacktrackingSimple;
-
-public class SolverBacktrackingSimple : ISudokuSolver
+namespace SolverBacktrackingSimple
 {
-private const int SIZE = 9;
-        private bool[,] rowUsed = new bool[SIZE, SIZE + 1];
-        private bool[,] colUsed = new bool[SIZE, SIZE + 1];
-        private bool[,] boxUsed = new bool[SIZE, SIZE + 1];
-        private int callCount = 0;
+    public class SolverBacktrackingSimple : ISudokuSolver
+    {
+        private const int SIZE = 9; // Taille de la grille de Sudoku
+        private const int EMPTY = 0; // Valeur représentant une case vide
 
         public SudokuGrid Solve(SudokuGrid s)
         {
-            InitializeConstraints(s);
-            callCount = 0;
-            if (Search(s, 0, 0))
+            int[] grid = ConvertGridToArray(s);
+            if (Solve(grid))
             {
-                Console.WriteLine("BacktrackingDotNetSolver: " + callCount + " search calls");
-                return s;
+                return ConvertArrayToGrid(grid);
             }
             else
             {
@@ -25,64 +22,114 @@ private const int SIZE = 9;
             }
         }
 
-        private void InitializeConstraints(SudokuGrid s)
+        private bool Solve(int[] grid)
         {
+            for (int i = 0; i < SIZE * SIZE; i++)
+            {
+                if (grid[i] == EMPTY)
+                {
+                    List<int> possibleNumbers = GetPossibleNumbers(i, grid);
+                    foreach (int num in possibleNumbers)
+                    {
+                        grid[i] = num;
+                        if (Solve(grid))
+                        {
+                            return true;
+                        }
+                        grid[i] = EMPTY;
+                    }
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private List<int> GetPossibleNumbers(int index, int[] grid)
+        {
+            if (grid[index] != EMPTY)
+            {
+                return new List<int>();
+            }
+            HashSet<int> possibleNumbers = new HashSet<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            possibleNumbers.ExceptWith(GetRowNumbers(index, grid));
+            possibleNumbers.ExceptWith(GetColumnNumbers(index, grid));
+            possibleNumbers.ExceptWith(GetSquareNumbers(index, grid));
+            return new List<int>(possibleNumbers);
+        }
+
+        private List<int> GetRowNumbers(int index, int[] grid)
+        {
+            int row = index / SIZE;
+            List<int> numbers = new List<int>();
+            for (int i = 0; i < SIZE; i++)
+            {
+                int cell = grid[row * SIZE + i];
+                if (cell != EMPTY)
+                {
+                    numbers.Add(cell);
+                }
+            }
+            return numbers;
+        }
+
+        private List<int> GetColumnNumbers(int index, int[] grid)
+        {
+            int col = index % SIZE;
+            List<int> numbers = new List<int>();
+            for (int i = 0; i < SIZE; i++)
+            {
+                int cell = grid[i * SIZE + col];
+                if (cell != EMPTY)
+                {
+                    numbers.Add(cell);
+                }
+            }
+            return numbers;
+        }
+
+        private List<int> GetSquareNumbers(int index, int[] grid)
+        {
+            int startRow = (index / SIZE) / 3 * 3;
+            int startCol = (index % SIZE) / 3 * 3;
+            List<int> numbers = new List<int>();
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    int cell = grid[(startRow + i) * SIZE + (startCol + j)];
+                    if (cell != EMPTY)
+                    {
+                        numbers.Add(cell);
+                    }
+                }
+            }
+            return numbers;
+        }
+
+        private int[] ConvertGridToArray(SudokuGrid s)
+        {
+            int[] grid = new int[SIZE * SIZE];
             for (int i = 0; i < SIZE; i++)
             {
                 for (int j = 0; j < SIZE; j++)
                 {
-                    int num = s.Cells[i, j];
-                    if (num != 0)
-                    {
-                        rowUsed[i, num] = true;
-                        colUsed[j, num] = true;
-                        boxUsed[GetBoxIndex(i, j), num] = true;
-                    }
+                    grid[i * SIZE + j] = s.Cells[i, j];
                 }
             }
+            return grid;
         }
 
-        private bool Search(SudokuGrid s, int row, int col)
+        private SudokuGrid ConvertArrayToGrid(int[] grid)
         {
-            callCount++;
-            if (col == SIZE)
+            SudokuGrid s = new SudokuGrid();
+            for (int i = 0; i < SIZE; i++)
             {
-                col = 0; ++row;
-                if (row == SIZE) return true;
-            }
-            if (s.Cells[row, col] != 0)
-                return Search(s, row, col + 1);
-
-            for (int num = 1; num <= SIZE; num++)
-            {
-                if (IsValid(row, col, num))
+                for (int j = 0; j < SIZE; j++)
                 {
-                    s.Cells[row, col] = num;
-                    SetConstraints(row, col, num, true);
-
-                    if (Search(s, row, col + 1)) return true;
-
-                    s.Cells[row, col] = 0;
-                    SetConstraints(row, col, num, false);
+                    s.Cells[i, j] = grid[i * SIZE + j];
                 }
             }
-            return false;
-        }
-
-        private bool IsValid(int row, int col, int num)
-        {
-            return !rowUsed[row, num] && !colUsed[col, num] && !boxUsed[GetBoxIndex(row, col), num];
-        }
-
-        private void SetConstraints(int row, int col, int num, bool state)
-        {
-            rowUsed[row, num] = state;
-            colUsed[col, num] = state;
-            boxUsed[GetBoxIndex(row, col), num] = state;
-        }
-
-        private int GetBoxIndex(int row, int col)
-        {
-            return (row / 3) * 3 + (col / 3);
+            return s;
         }
     }
+}
